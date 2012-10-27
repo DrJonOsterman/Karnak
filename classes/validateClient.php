c376d2c6b80809f66622082af501c349b819abf8
DrJonOsterman <egarcia2388@gmail.com>
2012-10-26 20:31:48 -0400

    meh

--- classes/validateClient.php
+++ classes/validateClient.php
@@ -6,13 +6,21 @@ $dab->connect();
 $val = new validate();
 
 
-$userUsername = $_REQUEST['formTxtUsername'];
-$userEmail = $_REQUEST['formTxtEmail'];
-$userPassword = $_REQUEST['formTxtPassword'];
 $type = $_REQUEST['param'];
+
+$username = $_REQUEST['formTxtUsername'];
+$email = $_REQUEST['formTxtEmail'];
+$password = $_REQUEST['formTxtPassword'];
+
 date_default_timezone_set('America/New_York');
 $userJoinDate = date('Y-m-d G:i:s', time());
 $userIP = $_SERVER['REMOTE_ADDR']; 
+
+
+if ($type === 'new') {newUserValidate();} //valdiate registration
+if ($type === 'edit') {echo 'soon';} 		// validate settings change
+if ($type === 'edit') {echo 'soon';} 		// validate log in credentials
+else if (!($type === 'new' || $type === 'edit')) {echo 'What are you doing here trickster? ;)';}
 
 
 
@@ -30,8 +38,5 @@ function newUserValidate()
 }
 
 
-if ($type === 'new') {newUserValidate();}
-if ($type === 'edit') {echo 'soon';}
-else if (!($type === 'new' || $type === 'edit')) {echo 'What are you doing here trickster? ;)';}
 
 ?>
